c92a9cfb0af3dc4012beab055ba83b0912987a92
swwayamm <codingswayam24@gmail.com>
2024-01-04 22:26:38 +0530

    Tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,42 @@ import { BtnComponent } from './components/btn/btn.component';
   standalone: true,
   imports: [CommonModule, RouterOutlet, BtnComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  wordCount = 0;
+  charCount = 0;
 
-  upperCase(element : any){
-    element.value = element.value.toUpperCase()    
+  upperCase(element: any) {
+    element.value = element.value.toUpperCase();
   }
 
-  lowerCase(element : any){
-    element.value = element.value.toLowerCase()
+  lowerCase(element: any) {
+    element.value = element.value.toLowerCase();
   }
 
-  charCount(element : any){
-    alert(element.value.length)
+  firstCaseCapital(element: any) {
+    
+  }
+
+  copy(element: any) {
+    navigator.clipboard.writeText(element.value)
+  }
+
+  getTextInfo(element: any) {
+    this.countChar(element);
+    this.countWord(element);
+  }
+
+  countWord(element: any) {
+    if (element.value == '') {
+      this.wordCount = 0;
+    } else {
+      this.wordCount = element.value.split(' ').length;
+    }
+  }
+
+  countChar(element: any) {
+    this.charCount = element.value.length;
   }
 }
